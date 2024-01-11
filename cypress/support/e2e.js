cc825fe4f571b34e9ba5dc7a0ef910665984d591
MuckT <mcgtom10@gmail.com>
2024-01-11 03:10:33 -0800

    style: run prettier

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -3,9 +3,13 @@
 /* eslint-disable consistent-return */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 // https://github.com/wagmi-dev/viem/discussions/781
-Cypress.on('uncaught:exception', (err, runnable) => {
+Cypress.on("uncaught:exception", (err, runnable) => {
   // returning false prevents Cypress from failing the test
-  if (err.message.includes('Chain "Celo" does not support contract "ensUniversalResolver"')) {
-    return false
+  if (
+    err.message.includes(
+      'Chain "Celo" does not support contract "ensUniversalResolver"',
+    )
+  ) {
+    return false;
   }
-})
+});
