4433125fc61ff98bcbe6f13e90a2b38ccc2307d1
MuckT <mcgtom10@gmail.com>
2024-01-10 21:45:11 -0800

    style: run prettier

--- src/main.tsx
+++ src/main.tsx
@@ -34,30 +34,30 @@ const contentTypeConfigs = [
 ];
 
 // Required field as of WalletConnect v2.
-const projectId = import.meta.env.VITE_PROJECT_ID
+const projectId = import.meta.env.VITE_PROJECT_ID;
 const metadata = {
-  name: 'Wallet DM',
-  description: 'A Chat App for Crypto Enthusiasts',
-  url: 'https://walletdm.com/',
-  icons: ['https://walletdm.com/favicon.ico']
-}
+  name: "Wallet DM",
+  description: "A Chat App for Crypto Enthusiasts",
+  url: "https://walletdm.com/",
+  icons: ["https://walletdm.com/favicon.ico"],
+};
 
 const chains = [celo, mainnet];
-const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
 
-createWeb3Modal({ 
-  wagmiConfig, 
-  projectId, 
+createWeb3Modal({
+  wagmiConfig,
+  projectId,
   chains,
   featuredWalletIds: [
     // Valora Wallet ID https://walletconnect.com/explorer/valora
-    'd01c7758d741b363e637a817a09bcf579feae4db9f5bb16f599fdd1f66e2f974',
+    "d01c7758d741b363e637a817a09bcf579feae4db9f5bb16f599fdd1f66e2f974",
     // Trust Wallet ID https://walletconnect.com/explorer/trust-wallet
-    '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
+    "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0",
     // MetaMask Wallet ID https://walletconnect.com/explorer/metamask
-    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
-  ] 
-})
+    "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
+  ],
+});
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <WagmiConfig config={wagmiConfig}>
@@ -66,5 +66,5 @@ createRoot(document.getElementById("root") as HTMLElement).render(
       dbVersion={DB_VERSION}>
       <App />
     </XMTPProvider>
-  </WagmiConfig>
+  </WagmiConfig>,
 );
