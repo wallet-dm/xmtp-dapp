4c55e52e03799933b66dd7cfb01a568247f9017d
MuckT <mcgtom10@gmail.com>
2024-01-11 02:49:35 -0800

    test(e2e): ignore console error for ensUniversalResolver

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,1 +1,10 @@
 // Required file for Cypress tests
+
+// https://github.com/wagmi-dev/viem/discussions/781
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false prevents Cypress from failing the test
+  if (err.message.includes('Chain "Celo" does not support contract "ensUniversalResolver"')) {
+    console.log('Ignoring ENS error on CELO 🚀')
+    return false
+  }
+})
